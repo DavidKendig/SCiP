cb47120fc8865d0ef02c934ab7b2551f107ea4d2
Trip <trip118.scifi@yahoo.com>
2021-12-30 22:59:48 -0500

    Initial commit

---
+++ src/SCiPnet/Core.java
@@ -0,0 +1,16 @@
+package SCiPnet;
+
+import java.util.*;
+
+public class Core {
+    private static String       serverIP    = "TEST";
+    private static String       ver         = "ALPHA";
+    private static String       developer   = "David Kendig";
+
+
+
+
+
+
+
+}
